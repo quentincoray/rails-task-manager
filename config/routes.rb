a1fa93dfce3cb9db473c74ec1574d52d4ee1d5d3
Quentin Coray <quentin.coray@gmail.com>
2017-03-07 12:25:35 +0100

    Migrated DB and added Task model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+
+  get 'pages/show'
+
+  get 'pages/new'
+
+  get 'pages/create'
+
+  get 'pages/edit'
+
+  get 'pages/update'
+
+  get 'pages/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tasks
 end
